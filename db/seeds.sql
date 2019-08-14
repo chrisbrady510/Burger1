346e7f38e4f055499899590d17d6a2370977b477
chrisbrady510 <chrismbrady@gmail.com>
2019-08-14 11:38:55 -0700

    done

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+### Populate data with 3 starter burgers
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheeseburger');
+
+INSERT INTO burgers (burger_name) VALUES ('Truffle Waffle Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Blue Cheese Bison Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Vegan Bacon and Cheese Burger');
